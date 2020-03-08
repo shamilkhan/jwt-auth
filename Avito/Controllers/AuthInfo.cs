91744a4d9ee6adaf9d2541ce3e64d014586b048e
shamilkhan <shamilkhan1999@gmail.com>
2020-03-08 15:19:45 +0300

    add initial commit

---
+++ Avito/Controllers/AuthInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Avito.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        /// <summary>
+        /// Инфа доступна, если пользователь залогинен
+        /// </summary>
+        [Authorize]
+        //[HttpGet]
+        public IActionResult GetLogin()
+        {
+            return Ok($"Ваш логин: {User.Identity.Name}");
+        }
+
+    }
+}
